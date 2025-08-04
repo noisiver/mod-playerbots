48a551228c1a4b41ada1efe897b0394cc5088280
Revision <tkn963@gmail.com>
2025-08-04 21:18:25 +0200

    Limit equipment equipped by altbots

--- src/strategy/values/ItemUsageValue.cpp
+++ src/strategy/values/ItemUsageValue.cpp
@@ -242,8 +242,47 @@ ItemUsage ItemUsageValue::QueryItemUsageForEquip(ItemTemplate const* itemProto, 
     if (itemProto->InventoryType == INVTYPE_NON_EQUIP)
         return ITEM_USAGE_NONE;
 
-    if (!sRandomPlayerbotMgr->IsRandomBot(bot) && itemProto->Class == ITEM_CLASS_WEAPON && itemProto->SubClass == ITEM_SUBCLASS_WEAPON_GUN)
-        return ITEM_USAGE_NONE;
+    if (!sRandomPlayerbotMgr->IsRandomBot(bot))
+    {
+        if (itemProto->Class == ITEM_CLASS_WEAPON && itemProto->SubClass == ITEM_SUBCLASS_WEAPON_GUN)
+            return ITEM_USAGE_NONE;
+
+        if (itemProto->Class == ITEM_CLASS_ARMOR)
+        {
+            switch (bot->getClass())
+            {
+                case CLASS_WARRIOR:
+                case CLASS_PALADIN:
+                case CLASS_DEATH_KNIGHT:
+                    if (itemProto->SubClass != ITEM_SUBCLASS_ARMOR_PLATE)
+                        return ITEM_USAGE_NONE;
+                    break;
+                case CLASS_HUNTER:
+                case CLASS_SHAMAN:
+                    if (itemProto->SubClass != ITEM_SUBCLASS_ARMOR_MAIL)
+                        return ITEM_USAGE_NONE;
+                    break;
+                case CLASS_ROGUE:
+                case CLASS_DRUID:
+                    if (itemProto->SubClass != ITEM_SUBCLASS_ARMOR_LEATHER)
+                        return ITEM_USAGE_NONE;
+                    break;
+                case CLASS_MAGE:
+                case CLASS_PRIEST:
+                case CLASS_WARLOCK:
+                    if (itemProto->SubClass != ITEM_SUBCLASS_ARMOR_CLOTH)
+                        return ITEM_USAGE_NONE;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (bot->getClass() == CLASS_SHAMAN && botAI->FindEquipSlot(itemProto, NULL_SLOT, true) == EQUIPMENT_SLOT_OFFHAND)
+            if (AiFactory::GetPlayerSpecTab(bot) != SHAMAN_TAB_ENHANCEMENT)
+                if (!(itemProto->Class == ITEM_CLASS_ARMOR && itemProto->SubClass == ITEM_SUBCLASS_ARMOR_SHIELD))
+                    return ITEM_USAGE_NONE;
+    }
 
     Item* pItem = Item::CreateItem(itemProto->ItemId, 1, bot, false, 0, true);
     if (!pItem)
