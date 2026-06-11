f3723d1898a691502482437153a112e636d55391
Noscopezz <mailzadop@gmail.com>
2026-06-11 09:50:33 +0200

    RS
    Strategies for RS
    
    Tested all bosses all diffs.
    
    For optimal results:
    10 Man 2 tanks 2 healers 3mdps 3 rdps
    25 Man 2 tanks 6 healers 8mdps 9rdps
    
    Player need to know tactics, since player can wipe raid if they fail to move away from group.

---
+++ src/Ai/Raid/RS/RSMultipliers.h
@@ -0,0 +1,76 @@
+#ifndef _PLAYERBOT_RSM_H
+#define _PLAYERBOT_RSM_H
+
+#include "Multiplier.h"
+
+class RsSavianaBeaconMultiplier : public Multiplier
+{
+public:
+    RsSavianaBeaconMultiplier(PlayerbotAI* ai) : Multiplier(ai, "rs saviana beacon") {}
+    virtual float GetValue(Action* action);
+};
+
+class RsBaltharusBrandSafeMultiplier : public Multiplier
+{
+public:
+    RsBaltharusBrandSafeMultiplier(PlayerbotAI* ai) : Multiplier(ai, "rs baltharus brand safe") {}
+    virtual float GetValue(Action* action);
+};
+
+class RsSavianaMeleeSpreadMultiplier : public Multiplier
+{
+public:
+    RsSavianaMeleeSpreadMultiplier(PlayerbotAI* ai) : Multiplier(ai, "rs saviana melee spread") {}
+    virtual float GetValue(Action* action);
+};
+
+class RsZarithrianAddsMultiplier : public Multiplier
+{
+public:
+    RsZarithrianAddsMultiplier(PlayerbotAI* ai) : Multiplier(ai, "rs zarithrian adds") {}
+    virtual float GetValue(Action* action);
+};
+
+class RsZarithrianTankSwapMultiplier : public Multiplier
+{
+public:
+    RsZarithrianTankSwapMultiplier(PlayerbotAI* ai) : Multiplier(ai, "rs zarithrian tank swap") {}
+    virtual float GetValue(Action* action);
+};
+
+class RsHalionCombustionMultiplier : public Multiplier
+{
+public:
+    RsHalionCombustionMultiplier(PlayerbotAI* ai) : Multiplier(ai, "rs halion combustion safe") {}
+    virtual float GetValue(Action* action);
+};
+
+class RsHalionMeteorMultiplier : public Multiplier
+{
+public:
+    RsHalionMeteorMultiplier(PlayerbotAI* ai) : Multiplier(ai, "rs halion meteor") {}
+    virtual float GetValue(Action* action);
+};
+
+class RsHalionMeleeFlankMultiplier : public Multiplier
+{
+public:
+    RsHalionMeleeFlankMultiplier(PlayerbotAI* ai) : Multiplier(ai, "rs halion melee flank") {}
+    virtual float GetValue(Action* action);
+};
+
+class RsHalionP2Multiplier : public Multiplier
+{
+public:
+    RsHalionP2Multiplier(PlayerbotAI* ai) : Multiplier(ai, "rs halion p2") {}
+    virtual float GetValue(Action* action);
+};
+
+class RsHalionHpBalanceMultiplier : public Multiplier
+{
+public:
+    RsHalionHpBalanceMultiplier(PlayerbotAI* ai) : Multiplier(ai, "rs halion hp balance") {}
+    virtual float GetValue(Action* action);
+};
+
+#endif
